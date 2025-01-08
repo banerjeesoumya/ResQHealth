54d3d926ff98b8160390f352af0f92344ffd38f9
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2025-01-08 16:31:12 +0530

    updating lib files

---
+++ resq-next/lib/items/sidebarItems.ts
@@ -0,0 +1,5 @@
+export const sidebarItems = [
+    { name: "Chatbot", href: "/chat" },
+    { name: "Stats", href: "/stats" },
+    { name: "Nearby Health Centers", href: "/findhealthcenter" },
+  ];
